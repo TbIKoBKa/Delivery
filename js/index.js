3a4a3a62acb44f44fe043b499b67447278bb33b6
TbIKoBKa <zhenya.mormul@gmail.com>
2021-11-19 23:38:51 +0200

    pr4.3

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const modalAuth = document.querySelector('#auth-modal')
 const inputLogin = document.querySelector('#login')
 const inputPassword = document.querySelector('#password')
 const messageAuthError = document.querySelector('#auth-error')
+const cardsWrapper = document.querySelector('#cards')
 
 let authStatus = JSON.parse(localStorage.getItem('authStatus'))
 let authData = JSON.parse(localStorage.getItem('authData'))
@@ -14,6 +15,85 @@ let users = [
     { login: "admin", password: "123" },
 ];
 let openStatus = false;
+
+const mockRestaurants = [
+    {
+        href: './restaurant.html',
+        img: './img/pizzaplus.jpg',
+        title: 'Пицца плюс',
+        tag: '50 мин',
+        rating: '4.5',
+        minPrice: 900,
+        category: 'Пицца'
+    },
+    {
+        href: './restaurant.html',
+        img: './img/tanuki.jpg',
+        title: 'Тануки',
+        tag: '50 мин',
+        rating: '4.5',
+        minPrice: 900,
+        category: 'Пицца'
+    },
+    {
+        href: './restaurant.html',
+        img: './img/foodband.jpg',
+        title: 'FoodBand',
+        tag: '50 мин',
+        rating: '4.5',
+        minPrice: 900,
+        category: 'Пицца'
+    },
+    {
+        href: './restaurant.html',
+        img: './img/jadinapizza.jpg',
+        title: 'Жадина-пицца',
+        tag: '50 мин',
+        rating: '4.5',
+        minPrice: 900,
+        category: 'Пицца'
+    },
+    {
+        href: './restaurant.html',
+        img: './img/tochkaedi.jpg',
+        title: 'Точка еды',
+        tag: '50 мин',
+        rating: '4.5',
+        minPrice: 900,
+        category: 'Пицца'
+    },
+    {
+        href: './restaurant.html',
+        img: './img/pizzaburger.jpg',
+        title: 'PizzaBurger',
+        tag: '50 мин',
+        rating: '4.5',
+        minPrice: 900,
+        category: 'Пицца'
+    },
+]
+
+mockRestaurants.forEach((mock) => {
+    cardsWrapper.insertAdjacentHTML('beforeend', `
+        <a href="${mock.href}" class="card" onclick="validateAuthorizeUser(event)">
+            <img src="${mock.img}" alt="image" class="card-image">
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title">${mock.title}</h3>
+                    <span class="card-tag tag">${mock.tag}</span>
+                </div>
+                <div class="card-info">
+                    <div class="rating">
+                        <img src="./img/star.svg" alt="rating" class="rating-star">
+                        ${mock.rating}
+                    </div>
+                    <div class="price">От ${mock.minPrice} ₽</div>
+                    <div class="category">${mock.category}</div>
+                </div>
+            </div>
+        </a>
+    `)
+})
 
 const toggleModalAuth = () => {
     modalAuth.classList.toggle('active');
@@ -60,6 +140,7 @@ buttonLogin.addEventListener('click', () => {
         if (user.login === inputLogin.value && user.password === inputPassword.value) {
             localStorage.setItem('authStatus', true);
             localStorage.setItem('authData', JSON.stringify(user));
+            authData = user
             authStatus = true;
             document.getElementById('login-text').innerText = inputLogin.value;
             
@@ -84,3 +165,15 @@ buttonLogin.addEventListener('click', () => {
         }, 3000)
     }
 })
+
+const validateAuthorizeUser = (event) => {
+    event.preventDefault()
+
+    console.log(authData)
+
+    if (authData?.login) {
+        console.log(window.location.href = event.currentTarget.href)
+    } else {
+        toggleModalAuth()
+    }
+}
